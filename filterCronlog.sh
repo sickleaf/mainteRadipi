05a371ccc93d380445d50d575a0d33a36c1caa92
sickleaf <5824061+sickleaf@users.noreply.github.com>
2019-09-14 11:54:45 +0900

    [fix] patch for succesive space (Sep 9 / Sep  10)

--- filterCronlog.sh
+++ filterCronlog.sh
@@ -5,6 +5,7 @@ filterNum=$2
 nowDate=`LANG=C; date '+%b %d %H:%M'` # Sep 12 23:42
 
 cat /var/log/cron.log | 
+sed -e 's/ \+/ /g' | # remove successive spaces
 grep " ${filterJob}" | # filter jobName
 tail -"${filterNum}" | # filter last N log
 awk -F'(' '{print $1, $3}' |  # extract TIME,JOB from cron.log
